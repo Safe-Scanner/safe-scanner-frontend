144b647dbcb99ec2442b56303be7eff9f34b6e54
Akash Rahman <akashrahman.me@gmail.com>
2023-12-08 17:00:05 +0600

    added balanace nft sub tabs

--- src/app/wallet/Balance.tsx
+++ src/app/wallet/Balance.tsx
@@ -3,11 +3,11 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Paper from "@mui/material/Paper";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import HashTab from "@/components/global/HashTab";
-import SmartRow from "@/components/global/DataTable/SmartRow";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import Image from "next/image";
+import Grid from "@mui/material/Grid";
 
 function Balance() {
   return (
@@ -15,34 +15,71 @@ function Balance() {
       <div>
         <HashTab size="small" tabs={["Crypto (7)", "NFTs (15)"]}>
           <Paper sx={{p: 3, mt: 2}}>
-            <Stack spacing={2.5}>
-              {[...Array(9)].map((_, i) => (
-                <Stack key={i} direction="row" spacing={1} alignItems="center">
-                  <Stack flexGrow={1}>
-                    <Typography
-                      variant="subtitle2"
-                      color="text.disabled"
-                      fontWeight="medium"
-                    >
-                      NFT {i + 1}
-                    </Typography>
-                    <Typography color="text.secondary" fontWeight="medium">
-                      0x242a…6b96
-                    </Typography>
+            <Grid container columnSpacing={4} rowSpacing={2.5}>
+              {[...Array(9)].map((_, index) => (
+                <Grid item key={index} xs={12} md={6} lg={4}>
+                  <Stack direction="row" spacing={1} alignItems="center">
+                    <Stack flexGrow={1}>
+                      <Typography
+                        variant="subtitle2"
+                        color="text.disabled"
+                        fontWeight="medium"
+                      >
+                        NFT {index + 1}
+                      </Typography>
+                      <Typography color="text.secondary" fontWeight="medium">
+                        0x242a…6b96
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={0.5} direction="row">
+                      <IconButton>
+                        <ContentCopyIcon color="primary" sx={{fontSize: 20}} />
+                      </IconButton>
+                      <IconButton href="/">
+                        <OpenInNewIcon color="primary" sx={{fontSize: 20}} />
+                      </IconButton>
+                    </Stack>
                   </Stack>
-                  <Stack spacing={0.5} direction="row">
-                    <IconButton>
-                      <ContentCopyIcon color="primary" sx={{fontSize: 20}} />
-                    </IconButton>
-                    <IconButton href="/">
-                      <OpenInNewIcon color="primary" sx={{fontSize: 20}} />
-                    </IconButton>
-                  </Stack>
-                </Stack>
+                </Grid>
               ))}
-            </Stack>
+            </Grid>
           </Paper>
-          <div>Comming Soon</div>
+          <Grid container sx={{marginTop: 2}} spacing={0.5}>
+            {[...Array(12)].map((_, index) => (
+              <Grid key={index} item xs={12} md={6} lg={4}>
+                <Paper sx={{padding: 2}}>
+                  <Stack spacing={2}>
+                    <Stack direction="row" spacing={2}>
+                      <Stack
+                        direction="row"
+                        spacing={2}
+                        flexGrow={1}
+                        alignItems="center"
+                      >
+                        <Image
+                          src="/images/ethereum.svg"
+                          alt=""
+                          width={40}
+                          height={40}
+                        />
+                        <Typography color="text.secondary">Ethereum</Typography>
+                      </Stack>
+                      <Stack>
+                        <Typography fontWeight="medium">2.45 ETH</Typography>
+                        <Typography
+                          color="text.disabled"
+                          variant="subtitle2"
+                          fontWeight="medium"
+                        >
+                          $4,163.44
+                        </Typography>
+                      </Stack>
+                    </Stack>
+                  </Stack>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
         </HashTab>
       </div>
     </Stack>
