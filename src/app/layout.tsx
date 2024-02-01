99bc2b6f84fcf5ae0496a7fd653829612d0aa7a8
-g <harshvardhan.rachiyata@gmail.com>
2024-02-01 15:42:42 +0530

    connect wallet provider connected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,35 @@ import type { Metadata } from "next";
 import "@/styles/main.css";
 import Layout from "@/components/Layout";
 import { Providers } from "@/store/provider";
+import { WalletProviders } from "./providers";
 
 export const metadata: Metadata = {
-    title: "Safe Scanner",
-    description: "User friendly transaction explorer for the Safe ecosystem",
+	title: "Safe Scanner",
+	description: "User friendly transaction explorer for the Safe ecosystem",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <body>
-                <Layout>
-                    <Providers>{children}</Providers>
-                </Layout>
-            </body>
-        </html>
-    );
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body>
+				<Layout>
+					<NewProvider>
+						<Providers>{children}</Providers>
+					</NewProvider>
+				</Layout>
+			</body>
+		</html>
+	);
 }
+
+const NewProvider = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<>
+			<WalletProviders>{children}</WalletProviders>
+		</>
+	);
+};
