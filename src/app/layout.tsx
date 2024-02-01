d798344a12832f5a991f64c26854a6d17e686fa4
skulshreshtha182 <skulshreshtha182@gmail.com>
2024-02-01 15:42:56 +0530

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,49 @@
-import type { Metadata } from "next";
+import React from "react";
 import "@/styles/main.css";
 import Layout from "@/components/Layout";
 import { Providers } from "@/store/provider";
+import "@rainbow-me/rainbowkit/styles.css";
 
-export const metadata: Metadata = {
-    title: "Safe Scanner",
-    description: "User friendly transaction explorer for the Safe ecosystem",
-};
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { createConfig, http, WagmiProvider } from "wagmi";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  zora,
+  sepolia,
+} from "wagmi/chains";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <body>
-                <Layout>
-                    <Providers>{children}</Providers>
-                </Layout>
-            </body>
-        </html>
-    );
+const { chains }: any = createConfig({
+  chains: [mainnet, polygon, optimism, arbitrum, base, zora, sepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+});
+
+const { connectors } = getDefaultWallets({
+  appName: "safe-scanner-test",
+  projectId: "79659658e5d42e7f152471a0d1e981b2",
+  chains,
+});
+
+const wagmiClient = createConfig({
+  autoConnect: true,
+  connectors,
+  // Define other wagmi client settings if needed
+});
+
+export default function RootLayout({ children }: any) {
+  return (
+    <WagmiProvider config={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Layout>
+          <Providers>{children}</Providers>
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiProvider>
+  );
 }
