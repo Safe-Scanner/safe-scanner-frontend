2afa5c66af75a75e399560788cd12c0c111cd65d
lazycoder1 <gautamgsabhahit@gmail.com>
2024-03-01 15:45:09 +0530

    adding posthog for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,32 +3,31 @@ import "@/styles/main.css";
 import Layout from "@/components/Layout";
 import { Providers } from "@/store/provider";
 // import { WalletProviders } from "./providers";
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
+import { CSPostHogProvider } from "./posthog";
 
-type Metadata =  {
-  title: string,
-  description:string
-}
-
-export const metadata: Metadata = {
-  title: "Safe Scanner",
-  description: "User friendly transaction explorer for the Safe ecosystem",
+type Metadata = {
+    title: string;
+    description: string;
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>
-        {/* <WalletProviders> */}
-          <Layout>
-            <Providers>{children}</Providers>
-          </Layout>
-        {/* </WalletProviders> */}
-      </body>
-    </html>
-  );
+export const metadata: Metadata = {
+    title: "Safe Scanner",
+    description: "User friendly transaction explorer for the Safe ecosystem",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body>
+                {/* <WalletProviders> */}
+                <CSPostHogProvider>
+                    <Layout>
+                        <Providers>{children}</Providers>
+                    </Layout>
+                </CSPostHogProvider>
+                {/* </WalletProviders> */}
+            </body>
+        </html>
+    );
 }
